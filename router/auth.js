c3295410436ad8e65ec3f6d014bca359d4942a60
okan batuk <okan.batuk07@gmail.com>
2020-01-03 04:20:12 +0300

    update

--- router/auth.js
+++ router/auth.js
@@ -4,20 +4,9 @@ const jwt = require('jsonwebtoken');
 const customerTransactions = require("../Database/query/customerTransactions");
 
 
-router.post('/register', async (req, res) => {
-    const response = await customerTransactions.RegisterUser(req.body);
-    if (response.recordset[0].Status != 0) {
-        res.json(response);
-    }
-    else {
-        res.json({ status: 500, message: "Not working transaction Register!" });
-    }
-})
-
 
 router.post('/login', async (req, res) => {
     const response = await customerTransactions.LoginUser(req.body)
-    console.log(response);
     if (response.recordset[0].Status != 0) {
         const payload = { tc: response.tc };
         const token = jwt.sign(payload, req.app.get('api_key'), { expiresIn: 720 });
